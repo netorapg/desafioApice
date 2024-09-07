39c5e71d88302d7ec66e5b9e072971b337db7d5a
netorapg <renatoplatz11@outlook.com>
2024-09-07 17:54:51 -0300

    checkpoint

--- src/pages/people/People.jsx
+++ src/pages/people/People.jsx
@@ -5,6 +5,7 @@ import { InputText } from 'primereact/inputtext';
 import { FloatLabel } from 'primereact/floatlabel';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import { Dropdown } from 'primereact/dropdown';
 import 'primeflex/primeflex.css';
 
 export default function Pessoa() {
@@ -22,8 +23,8 @@ export default function Pessoa() {
     });
 
     const [people, setPeople] = useState([]); // Estado para armazenar as pessoas
-    const [cidades, setCidades] = useState([]); // Estado para armazenar as cidades
-    const [bairros, setBairros] = useState([]); // Estado para armazenar os bairros
+    const [cities, setCities] = useState([]); // Estado para armazenar as cidades
+    const [neighborhoods, setNeighborhoods] = useState([]); // Estado para armazenar os bairros
     const [editingId, setEditingId] = useState(null); // Estado para armazenar o ID da pessoa em edição
 
     const navigate = useNavigate();
@@ -39,30 +40,32 @@ export default function Pessoa() {
         }
     };
 
-    const fetchCidades = async () => {
+    // Função para buscar cidades
+    const fetchCities = async () => {
         try {
             const response = await fetch('http://localhost:3001/api/cidades');
             const data = await response.json();
-            setCidades(data);
+            setCities(data);
         } catch (error) {
             console.error('Error fetching cities:', error);
         }
     };
 
-    const fetchBairros = async () => {
+    // Função para buscar bairros
+    const fetchNeighborhoods = async () => {
         try {
             const response = await fetch('http://localhost:3001/api/bairros');
             const data = await response.json();
-            setBairros(data);
+            setNeighborhoods(data);
         } catch (error) {
             console.error('Error fetching neighborhoods:', error);
         }
     };
 
     useEffect(() => {
-        fetchPeople();
-        fetchCidades();
-        fetchBairros();
+        fetchPeople(); // Chamando a função para buscar os dados das pessoas ao carregar o componente
+        fetchCities(); // Chamando a função para buscar os dados das cidades ao carregar o componente
+        fetchNeighborhoods(); // Chamando a função para buscar os dados dos bairros ao carregar o componente
     }, []);
 
     const handleChange = (e) => {
@@ -73,7 +76,7 @@ export default function Pessoa() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log('Dados enviados:', formData); // Log dos dados enviados
-
+    
         // Verifique se todos os campos necessários estão preenchidos
         if (!formData.codigo || !formData.nome || !formData.cep || !formData.endereco || !formData.numero || !formData.telefone || !formData.email || !formData.cidade_id || !formData.bairro_id) {
             alert('Por favor, preencha todos os campos.');
@@ -84,7 +87,7 @@ export default function Pessoa() {
         const url = editingId 
             ? `http://localhost:3001/api/pessoas/${editingId}` 
             : 'http://localhost:3001/api/pessoas';
-
+    
         try {
             const response = await fetch(url, {
                 method,
@@ -104,7 +107,7 @@ export default function Pessoa() {
                     bairro_id: formData.bairro_id
                 }),
             });
-
+    
             if (response.ok) {
                 alert(editingId ? 'Pessoa atualizada com sucesso!' : 'Pessoa adicionada com sucesso!');
                 setFormData({
@@ -159,7 +162,7 @@ export default function Pessoa() {
                 const response = await fetch(`http://localhost:3001/api/pessoas/${personId}`, {
                     method: 'DELETE',
                 });
-
+    
                 if (response.ok) {
                     alert('Pessoa deletada com sucesso!');
                     setPeople(prevPeople => prevPeople.filter(person => person.id !== personId));
@@ -178,7 +181,7 @@ export default function Pessoa() {
                 alert('Erro ao deletar pessoa');
             }
         }
-    };
+    }
 
     const handleCancel = () => {
         setFormData({
@@ -227,7 +230,7 @@ export default function Pessoa() {
                             ))}
                         </div>
                     </TabPanel>
-                    <TabPanel header={editingId ? "Editar" : "Incluir"} leftIcon="pi pi-user-plus mr-2">
+                    <TabPanel header="Incluir" leftIcon="pi pi-user-plus mr-2">
                         <form onSubmit={handleSubmit}>
                             <div className="formgrid grid mb-4">
                                 <div className="col-12 md:col-6 lg:col-2">
@@ -238,7 +241,6 @@ export default function Pessoa() {
                                             value={formData.codigo}
                                             onChange={handleChange}
                                             style={{ width: '100%' }}
-                                            disabled={editingId !== null} // Desabilitar o campo "Código" ao editar
                                         />
                                         <label htmlFor="codigo">Código</label>
                                     </FloatLabel>
@@ -329,37 +331,27 @@ export default function Pessoa() {
                                 </div>
                                 <div className="col-12 md:col-6 lg:col-2">
                                     <FloatLabel>
-                                        <select
-                                            id="cidade_id"
+                                        <Dropdown
                                             value={formData.cidade_id}
-                                            onChange={handleChange}
+                                            options={cities}
+                                            optionLabel="nome"
+                                            placeholder="Selecione uma cidade"
+                                            onChange={(e) => setFormData(prevData => ({ ...prevData, cidade_id: e.value.id }))}
                                             style={{ width: '100%' }}
-                                        >
-                                            <option value="">Selecione a Cidade</option>
-                                            {cidades.map(cidade => (
-                                                <option key={cidade.id} value={cidade.id}>
-                                                    {cidade.nome}
-                                                </option>
-                                            ))}
-                                        </select>
+                                        />
                                         <label htmlFor="cidade_id">Cidade</label>
                                     </FloatLabel>
                                 </div>
                                 <div className="col-12 md:col-6 lg:col-2">
                                     <FloatLabel>
-                                        <select
-                                            id="bairro_id"
+                                        <Dropdown
                                             value={formData.bairro_id}
-                                            onChange={handleChange}
+                                            options={neighborhoods}
+                                            optionLabel="nome"
+                                            placeholder="Selecione um bairro"
+                                            onChange={(e) => setFormData(prevData => ({ ...prevData, bairro_id: e.value.id }))}
                                             style={{ width: '100%' }}
-                                        >
-                                            <option value="">Selecione o Bairro</option>
-                                            {bairros.map(bairro => (
-                                                <option key={bairro.id} value={bairro.id}>
-                                                    {bairro.nome}
-                                                </option>
-                                            ))}
-                                        </select>
+                                        />
                                         <label htmlFor="bairro_id">Bairro</label>
                                     </FloatLabel>
                                 </div>
